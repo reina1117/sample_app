3ce60c01ce52a2bb300ea56a31774da888257d61
reina1117 <122454145+reina1117@users.noreply.github.com>
2023-02-15 14:16:32 +0000

    [Finish] 3-4

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,26 +3,24 @@ class ListsController < ApplicationController
     # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
     @list = List.new
   end
-  
+
   def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
     list = List.new(list_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
     list.save
-    # 4. トップ画面へリダイレクト
-    redirect_to '/top'
+    redirect_to list_path(list.id)
   end
 
   def index
-     @lists = List.all
+    @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
   end
-  
+
   private
   def list_params
     params.require(:list).permit(:title, :body)
